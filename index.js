3c426ea3921190f7d54979be00cd4f582dfbe6ce
david1707 <DavidAlacant@gmail.com>
2018-12-16 19:27:00 +0100

    Added axes and colour to bars based on their height

--- index.js
+++ index.js
@@ -16,8 +16,16 @@ const graph = svg.append('g')
     .attr('height', graphHeight)
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
+// Information axes
+const xAxisGroup = graph.append('g')
+    .attr('transform', `translate(0, ${graphHeight})`);
+
+const yAxisGroup = graph.append('g');
+
 // Load JSON
 d3.json('techs.json').then(data => {
+
+    // X and Y scalers
     const x = d3.scaleBand()
         .domain(data.map(d => d.tech))
         .range([0, graphWidth])
@@ -25,8 +33,12 @@ d3.json('techs.json').then(data => {
         .paddingOuter('0.3')
 
     const y = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.level)])
+        .domain([0, 100])
         .range([graphHeight, 0])
+
+    // Colours
+    const colour = d3.scaleSequential(d3.interpolateBuGn)
+        .domain([0, 100])
 
     const rects = graph.selectAll('rect')
         .data(data)
@@ -36,7 +48,7 @@ d3.json('techs.json').then(data => {
         .attr('height', d => graphHeight - y(d.level))
         .attr('x', d => x(d.tech))
         .attr('y', d => y(d.level))
-        .attr('fill', 'red')
+        .attr('fill', (d, i) => colour(d.level))
 
     // Add new rects to the DOM
     rects.enter()
@@ -45,5 +57,20 @@ d3.json('techs.json').then(data => {
             .attr('height', d => graphHeight - y(d.level))
             .attr('x', d => x(d.tech))
             .attr('y', d => y(d.level))
-            .attr('fill', 'red')
+            .attr('fill', (d, i) => colour(d.level))
+    
+    // Create axes
+    const xAxis = d3.axisBottom(x)
+    const yAxis = d3.axisLeft(y)
+        .ticks(20)
+        .tickFormat(d => d + ' %');
+
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+
+    xAxisGroup.selectAll('text')
+        .attr('fill', 'black')
+        .attr('transform', 'translate(0, 10)')
+        .attr('text-anchor', 'center')
+    
 });
